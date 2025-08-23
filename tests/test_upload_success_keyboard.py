597ab8f80ea7641e6462656ede4c89341ab3e91c
Onewave-lang <languageswithchatgpt@gmail.com>
2025-08-23 11:09:10 +0200

    feat: extend photo upload success menu

--- tests/test_upload_success_keyboard.py
+++ tests/test_upload_success_keyboard.py
@@ -3,10 +3,12 @@ import app
 
 def test_upload_success_keyboard_buttons():
     kb = app.upload_success_keyboard()
-    assert len(kb.inline_keyboard) == 3
-    assert kb.inline_keyboard[0][0].text == "Добавить ещё фото"
+    assert len(kb.inline_keyboard) == 4
+    assert kb.inline_keyboard[0][0].text == "Добавить ещё фото для этого айдола"
     assert kb.inline_keyboard[0][0].callback_data == app.CB_UPLOAD_MORE
-    assert kb.inline_keyboard[1][0].text == "📁 В каталог фото"
-    assert kb.inline_keyboard[1][0].callback_data == "menu_catalog"
-    assert kb.inline_keyboard[2][0].text == "⬅️ Назад в меню"
-    assert kb.inline_keyboard[2][0].callback_data == "menu_back"
+    assert kb.inline_keyboard[1][0].text == "Добавить для других айдолов"
+    assert kb.inline_keyboard[1][0].callback_data == app.CB_UPLOAD_OTHER
+    assert kb.inline_keyboard[2][0].text == "📁 В каталог фото"
+    assert kb.inline_keyboard[2][0].callback_data == "menu_catalog"
+    assert kb.inline_keyboard[3][0].text == "⬅️ Назад в меню"
+    assert kb.inline_keyboard[3][0].callback_data == "menu_back"
